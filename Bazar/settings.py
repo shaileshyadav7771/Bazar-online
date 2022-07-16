14d49f06aebdaeeac866bb94f2818e87be17aba0
shaileshyadav7771 <shaileshyadav7958@gmail.com>
2022-07-16 16:33:30 +0530

    home image..

--- Bazar/settings.py
+++ Bazar/settings.py
@@ -130,11 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT= BASE_DIR /'static'
-STATICFILES_DIRS=[
-    'Bazar/static',
-]
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    'Bazar/static/',
+]
 
 # Media File Configuration
 
