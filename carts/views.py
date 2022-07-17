dce5069441eb982e2fbd6ec60a15885abcd15d2b
shaileshyadav7771 <shaileshyadav7958@gmail.com>
2022-07-17 14:42:58 +0530

    Removing an item from added card list..

--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product
 from .models import Cart, CartItem
 
@@ -39,6 +39,26 @@ def add_cart(request, product_id):
     cart_item.save()
     return redirect('cart')
 
+#For removing Card
+def remove_cart(request, product_id):
+    #we need to get cart
+    cart = Cart.objects.get(Cart_id = _cart_id(request))
+    product = get_object_or_404(Product , id = product_id)
+    cart_item = CartItem.objects.get(product=product , cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+#for deleting card Item
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(Cart_id = _cart_id(request))
+    product = get_object_or_404(Product , id = product_id)
+    cart_item = CartItem.objects.get(product=product , cart=cart)
+    cart_item.delete()
+    return redirect('cart')
 
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
