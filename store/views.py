a6f0a0f7fc2f275e15c2497a99cb43bab2ba1490
shaileshyadav7771 <shaileshyadav7958@gmail.com>
2022-07-17 17:25:34 +0530

    Check if already added to cart then dispaly view button.

--- store/views.py
+++ store/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
-
+from carts.views import _cart_id
+from carts.models import CartItem
 
 # Create your views here.
 
@@ -27,11 +28,15 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
-        print(category_slug, product_slug)
-        print("single_product :", single_product)
+        print(" single_product :", single_product)
+        #logic for what if card already added in detail page (we should not show add card again.)
+        in_cart = CartItem.objects.filter(cart__Cart_id=_cart_id(request),product=single_product).exists()
+        print("in_cart : ", in_cart)
 
     except Exception as e:
         raise e
-    context = {'single_product': single_product, }
+    context = {'single_product': single_product,
+               'in_cart' : in_cart,
+               }
 
     return render(request, 'store/product_detail.html', context)
